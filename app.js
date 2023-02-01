bfb53bd37c1861c076befdb6233c7f28217e764b
jubair <jubair@jubairs-MacBook-Pro.local>
2023-02-02 00:11:02 +0600

    new change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome')
+    res.send('Welcome to home')
 })
 
 app.listen(5001, function() {
